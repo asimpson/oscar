79d5ffae79b2e879be8ec71f9b65f037981da331
Adam Simpson <adam@heysparkbox.com>
2020-08-02 22:32:54 -0400

    chore: fix travis again

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-rustup target add "$TARGET"
+rustup target add "${TARGET}"
 
 # Download the Raspberry Pi cross-compilation toolchain if needed
-if [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]
+if [ "${TARGET}" = "arm-unknown-linux-gnueabihf" ]
 then
   git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools;
-  export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH;
+  export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:${PATH};
   export CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
 fi
 
-cargo build --target="$TARGET" --release
+cargo build --target="${TARGET}" --release
