19e1a1f8cbfc1e61c8fc9635be7676b3d01871e7
Adam Simpson <adam@adamsimpson.net>
2020-08-02 20:07:56 -0400

    chore: travis build changes

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rustup target add "$TARGET"
 # Download the Raspberry Pi cross-compilation toolchain if needed
 if [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]
 then
-  git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools
-  export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH
+  git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools;
+  export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH;
 fi
 
 cargo build --target="$TARGET" --release
