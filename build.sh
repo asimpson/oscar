8ced54e236585b8c8faab247a3da59bdcbe32b0d
Adam Simpson <adam@adamsimpson.net>
2020-08-02 20:29:23 -0400

    chore: forgot linker env for raspi

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if [ "$TARGET" = "arm-unknown-linux-gnueabihf" ]
 then
   git clone --depth=1 https://github.com/raspberrypi/tools.git /tmp/tools;
   export PATH=/tmp/tools/arm-bcm2708/arm-linux-gnueabihf/bin:$PATH;
+  export CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER=arm-linux-gnueabihf-gcc
 fi
 
 cargo build --target="$TARGET" --release
